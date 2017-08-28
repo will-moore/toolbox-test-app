07fe4ea028fc4f265d11ede8460a66a5b306f63a
William Moore <w.moore@dundee.ac.uk>
2017-08-28 22:18:59 +0100

    Initial commit with failing theme.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import theme from 'assets/react-toolbox/theme'
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+
+ReactDOM.render(
+	<ThemeProvider theme={theme}>
+		<App />
+	</ThemeProvider>, document.getElementById('root'));
+registerServiceWorker();
