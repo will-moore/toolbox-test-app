781760bb7e6285cbfdda14f10a5310657660ce1c
William Moore <w.moore@dundee.ac.uk>
2017-08-30 22:08:40 +0100

    Fixed react-toolbox theme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import theme from 'assets/react-toolbox/theme'
+import theme from './assets/react-toolbox/theme'
+import './assets/react-toolbox/theme.css'
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 
 ReactDOM.render(
